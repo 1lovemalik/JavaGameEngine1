4c15d37fad2274736d1e96ac3cc0b92b6a7332ff
Malik Diallo <ahamedmalikd@gmail.com>
2024-11-17 01:47:10 -0500

    First Commit

---
+++ src/GamePanel.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class GamePanel extends JPanel {
+    // SCREEN SETTINGS
+    private final int originalTileSize = 16;
+    private final int scale = 3;
+
+    private final int tileSize = originalTileSize * scale;
+    private final int maxScreenCol = 16;
+    private final int maxScreenRow = 12;
+    private final int screenWidth = tileSize * maxScreenCol;
+
+}
