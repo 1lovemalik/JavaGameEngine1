7c0923139c2f18722195681ad70ec4a1a1aa48e3
Malik Diallo <ahamedmalikd@gmail.com>
2024-11-17 02:07:19 -0500

    changes

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
     // SCREEN SETTINGS
     private final int originalTileSize = 16;
     private final int scale = 3;
@@ -12,10 +12,22 @@ public class GamePanel extends JPanel {
     private final int screenWidth = tileSize * maxScreenCol; // 768px
     private final int screenHeight = tileSize * maxScreenRow; // 576px
 
+    Thread gameThread;
+
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
     }
 
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
